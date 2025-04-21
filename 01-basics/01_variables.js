bd9b60f1e37e51c58cadaf145652e30a12863cb3
Saifullah085 <119309836+Saifullah085@users.noreply.github.com>
2025-04-21 09:00:16 +0000

    tryin to save

---
+++ 01-basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553;
+let accountEmail = "saifullah@gmail.com";
+var accountPassword = "12345";
+accountCity = "nawabshah";
+
+// accountId = 2; //not allowed
+
+accountEmail = "theyaskme@sa.com";
+accountPassword ="6789";
+accountCity = "serari";
+
+console.log(accountEmail);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+/*
+prefer not to use var 
+because of isuue in block scope and function scope
+
+*/
