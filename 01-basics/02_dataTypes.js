9f13db66a8b5dd855f96b7c53cd75ce2478a086f
Saifullah085 <119309836+Saifullah085@users.noreply.github.com>
2025-04-21 09:45:37 +0000

    datatypes

---
+++ 01-basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict" //treat all js code as  newer version
+
+// alert(4+5);  //we are using nodejs , not browser
+
+// Primitive DataTypes
+// number => 2 to power 53
+// bigint
+// string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+
+//object
+
+console.log(typeof null);
