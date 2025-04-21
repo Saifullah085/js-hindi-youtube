5f1e2bcc1ba2c6229abb0191e50f64fc71f01c9e
Saifullah085 <119309836+Saifullah085@users.noreply.github.com>
2025-04-21 17:12:29 +0000

    comparison

---
+++ 01-basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1)
+
+// console.log(null > 0); //means in equality it works different and in comparison like less than or greater than it takes null is equal to 0
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+//  ===
+
+console.log("2" === 2);
+
