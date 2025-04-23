f1e1b341a5ca1bbb9177fb020cd2d77784315b49
Saifullah085 <119309836+Saifullah085@users.noreply.github.com>
2025-04-23 07:55:23 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,25 @@
+//array
+
+const myArr = [0, 1, 2, 3, 4, 5];
+const myHeroes = ["shaktiman", "naagraj"]
+
+const myarr2 = new Array(1, 2, 3, 4);
+console.log(myArr[0]);
+
+// Array methods
+
+// myArr.push(6);
+// myArr.push(7);
+// myArr.pop();
+
+// myArr.unshift(7);
+// myArr.shift();
+
+// console.log(myArr.includes(4));
+// console.log(myArr.indexOf(2));
+
+const newArr = myArr.join()
+
+console.log(myArr);
+console.log(typeof newArr);
+
