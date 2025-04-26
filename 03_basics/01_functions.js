b42bd0fb87e8d9466afec95f4db4c922519c25b9
Saifullah085 <119309836+Saifullah085@users.noreply.github.com>
2025-04-26 06:54:41 +0000

    function and scopes

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -27,4 +27,36 @@ function loginUserMessage(username){
 }
 
 // console.log(loginUserMessage("saifullah"));
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
+
+
+function calculateCartPrice(val1, val2, ...num1) {
+    return num1;
+}
+
+// console.log(calculateCartPrice(200,400,500,2000));
+
+const user = {
+    username: "saifullah",
+    price: 199
+}
+
+function handleObject(anyobject) {
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+
+// handleObject(user);
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600];
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,400,500,1000]));
